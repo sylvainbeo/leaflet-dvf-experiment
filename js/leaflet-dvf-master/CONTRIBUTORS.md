e9d6f355b9b93bf79ac7287f7c1b614014e824fa
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2014-01-14 14:54:35 +0100

    POIs replaced by charts with DVF lib

---
+++ js/leaflet-dvf-master/CONTRIBUTORS.md
@@ -0,0 +1,14 @@
+leaflet-dvf Contributors
+==================
+
+Leaflet DVF is made possible by contributions from:
+
+* Scott Fairgrieve [sfairgrieve](https://github.com/sfairgrieve)
+* Keith Chew [keithchew](https://github.com/keithchew)
+* Jeff Meadows [Jeff-Meadows](https://github.com/Jeff-Meadows)
+* Rob Story [wrobstory](https://github.com/wrobstory)
+* Glen Robertson [glenrobertson](https://github.com/glenrobertson)
+* Jelle Victoor [JelleConundra](https://github.com/JelleConundra)
+* Patrick Doody [go0ty](https://github.com/go0ty)
+* Franck Tabary [franck34] (https://github.com/franck34)
+
--- js/main.js
+++ js/main.js
@@ -10,15 +10,50 @@ $( document ).ready(function() {
     $.getJSON(url, function(data) {
         geojsonLayer = L.geoJson(data, {
             pointToLayer: function (feature, latlng) {
-                var geojsonMarkerOptions = {
-                    radius: 8,
-                    fillColor: "#ff7800",
-                    color: "#000",
+                var options = {
+                    data: {
+                        'data1': feature.data1,
+                        'data2': feature.data2,
+                        'data3': feature.data3
+                    },
+                    chartOptions: {
+                        'data1': {
+                            fillColor: '#FF0000',
+                            minValue: 0,
+                            maxValue: 100,
+                            maxHeight: 100,
+                            displayText: function (value) {
+                                return value + "%";
+                            }
+                        },
+                        'data2': {
+                            fillColor: '#0000FF',
+                            minValue: 0,
+                            maxValue: 100,
+                            maxHeight: 100,
+                            displayText: function (value) {
+                                return value + "%";
+                            }
+                        },
+                        'data3': {
+                            fillColor: '#00FF00',
+                            minValue: 0,
+                            maxValue: 100,
+                            maxHeight: 100,
+                            displayText: function (value) {
+                                return value + "%";
+                            }
+                        }
+                    },
                     weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                };
-                return L.circleMarker(latlng, geojsonMarkerOptions);
+                    color: '#666',
+                    opacity: 1
+                }
+                var barChartMarker = new L.BarChartMarker(latlng, options);
+                //var barChartMarker = new L.RadialBarChartMarker(latlng, options);
+                //var barChartMarker = new L.StackedRegularPolygonMarker(latlng, options);
+                //var barChartMarker = new L.PieChartMarker(latlng, options);    
+                return barChartMarker;
             }
         });
         geojsonLayer.addTo(map);
